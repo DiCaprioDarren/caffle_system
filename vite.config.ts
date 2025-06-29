333078e896304815e19ec64a21c3d02368089f41
DiCaprioDarren <1136455707@qq.com>
2025-06-29 21:08:33 +0800

    all:修改文件使得github能部署上线

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/caffle_system/' : '/',
   plugins: [vue()],
-  server:{
-    port: 8080
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    }
   }
 })
